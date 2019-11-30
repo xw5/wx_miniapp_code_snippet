f3d226ac18026a78fab024ecd6343dfb9a5016c7
xw5 <412832768@qq.com>
2019-11-30 17:46:30 +0800

    优化激励广告组件

--- create_rewarded_video_ad/src/create_rewarded_video_ad.js
+++ create_rewarded_video_ad/src/create_rewarded_video_ad.js
@@ -5,12 +5,12 @@ class RwwardVideoAd {
   closeCallBack = null;    // 看完广告正常关闭回调
   closeMidCallBack = null; // 未看完广告就点击了关闭
   showErrorCallBack = null; // 广告播放错误回调
-  adLoadStatusBack = null;  // 广告加载回调成功失败都会
+  adLoadStatusBack = null;  // 广告加载状态回调，成功失败都会
 
   /**
    * 
    * @param {string} id 广告id
-   * @param {boolean} isMultiton 是否开启广告多例模式
+   * @param {boolean} isMultiton 是否开启广告多例模式 微信使用
    */
   constructor(id, isMultiton) {
     this.init(id, isMultiton)
@@ -23,20 +23,13 @@ class RwwardVideoAd {
       this.rewardedVideoAd = null;
       // 初始化一个视频广告
       this.rewardedVideoAd = wx.createRewardedVideoAd({adUnitId: this.rewardVideoAdId, multiton: isMultiton});
-      // 广告加载成功回调
-      this.rewardedVideoAd.onLoad(() => {
-        this.chagneStatus(true);
-      });
+      
+      this.bindEventLoad();
 
-      // 广告加载失败回调
-      this.rewardedVideoAd.onError((err) => {
-        this.chagneStatus(false);
-      });
+      this.bindEventError();
 
-      // 点击关闭按钮触发事件监听
-      this.rewardedVideoAd.onClose((res) => {
-        this.closeFn(res);
-      });
+      // 主动加载一次广告，避免首次获取不到广告状态
+      this.reload();
       console.log("视频激励广告初始化：", this.rewardVideoAdId, this.rewardedVideoAd);
     } else {
       console.log("当前小程序版本不支持激励视频广告组件！");
@@ -49,24 +42,23 @@ class RwwardVideoAd {
    */
   chagneStatus(mark) {
     this.isSuccess = mark;
-    this.adLoadStatusBack && this.adLoadStatusBack(mark);
-    console.log('视频广告更新：', mark);
+    this.adLoadStatusBack && this.adLoadStatusBack(this.rewardVideoAdId, mark);
+    console.log('视频广告更新：', mark, this.rewardVideoAdId);
   }
 
   /**
-   * 
+   * @param {string} id 广告id
    * @param {object} res 激励视频关闭回调
    */
-  closeFn(res) {
+  closeFn(id, res) {
     // 用户点击了【关闭广告】按钮
     // 小于 2.1.0 的基础库版本，res 是一个 undefined
     if (res && res.isEnded || res === undefined) {
       // 正常播放结束，可以下发游戏奖励
-      this.closeCallBack && this.closeCallBack();
-      //this.clearCallBack();
+      this.closeCallBack && this.closeCallBack(this.rewardVideoAdId);
     } else {
       // 播放中途退出，不下发游戏奖励
-      this.closeMidCallBack && this.closeMidCallBack();
+      this.closeMidCallBack && this.closeMidCallBack(this.rewardVideoAdId);
     }
   }
 
@@ -78,47 +70,64 @@ class RwwardVideoAd {
   }
 
   /**
-   * 清除所有绑定的事件
+   * 绑定load监听事件
    */
-  clearAll() {
-    this.closeCallBack = null;
-    this.closeMidCallBack = null;
-    this.showErrorCallBack = null;
-    this.adLoadStatusBack = null;
+  bindEventLoad() {
+    // 广告加载成功回调
+    this.rewardedVideoAd.onLoad(() => {
+      console.log("load是否触发成功了。。。");
+      this.chagneStatus(true);
+    });
   }
 
   /**
-   * 清除正常关闭回调
+   * 绑定error监听事件
    */
-  clearCallBack() {
-    this.closeCallBack = null;
+  bindEventError() {
+    // 广告加载失败回调
+    this.rewardedVideoAd.onError((err) => {
+      console.log("error是否触发成功了。。。");
+      this.chagneStatus(false);
+    });
   }
 
   /**
-   * 清除未看完广告关闭回调
+   * 绑定close监听事件
    */
-  clearMidCallBack() {
-    this.closeMidCallBack = null;
+  bindEventClose() {
+    // 点击关闭按钮触发事件监听
+    this.rewardedVideoAd.onClose((res) => {
+      this.closeFn(this.rewardedVideoAd, res);
+      this.removeEventClose();
+    });
   }
 
   /**
-   * 清除播放广告失败回调
+   * 删除事件监听
    */
-  clearErrorCallBack() {
-    this.showErrorCallBack = null;
+  removeEventLoad() {
+    this.rewardedVideoAd.offLoad();
   }
 
   /**
-   * 清除广告状态通知回调
+   * 删除事件监听
    */
-  clearAdLoadStatusBack() {
-    this.adLoadStatusBack = null;
+  removeEventError() {
+    this.rewardedVideoAd.offError();
+  }
+
+  /**
+   * 删除事件监听
+   */
+  removeEventClose() {
+    this.rewardedVideoAd.offClose();
   }
 
   /** 
    * @param 播放激励视频广告
    */
   show() {
+    this.bindEventClose();
     this.rewardedVideoAd.show().then(() => {}).catch(() => {
       this.showErrorCallBack && this.showErrorCallBack();
       wx.showToast({
